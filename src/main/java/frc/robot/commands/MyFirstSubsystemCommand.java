af12d31070776f90947c42ebcaf711d0ed0417ec
razco1212 <raztektok@gmail.com>
2025-06-30 23:26:11 +0300

    Implement MyFirstSubsystemCommand with power control and duration handling

--- src/main/java/frc/robot/commands/MyFirstSubsystemCommand.java
+++ src/main/java/frc/robot/commands/MyFirstSubsystemCommand.java
@@ -4,30 +4,45 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.MyFirstSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class MyFirstSubsystemCommand extends Command {
-  /** Creates a new MyFirstSubsystemCommand. */
-  public MyFirstSubsystemCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private final MyFirstSubsystem subsystem;
+  private final double power; private final double duration; private double startTime;
+
+  public MyFirstSubsystemCommand (MyFirstSubsystem subsystem, double power, double duration) {
+    this.subsystem = subsystem;
+    this.power = power;
+    this.duration = duration;
+    addRequirements(subsystem);
   }
   
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    startTime = Timer.getFPGATimestamp();
+    System.out.println("Command started at: " + startTime + " seconds for "+ duration + " seconds with power: " + power);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
+  public void execute() {
+    subsystem.setPower(power);
+  }
   @Override
   public boolean isFinished() {
-    return false;
+    return Timer.getFPGATimestamp() >= startTime + duration;
   }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    subsystem.stop();
+    System.out.println("Command ended at: " + Timer.getFPGATimestamp());
+  }
+
+  // Returns true when the command should end.
+
 }
