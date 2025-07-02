5cfecfc59134b655bd5c55acba221b0d8e10d72a
razco1212 <raztektok@gmail.com>
2025-07-02 21:42:30 +0300

    Add import for MotorType in MyFirstSubsystem

--- src/main/java/frc/robot/subsystems/MyFirstSubsystem.java
+++ src/main/java/frc/robot/subsystems/MyFirstSubsystem.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
