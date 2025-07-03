5b1f4a97c61a50eb7a880cffb56bfa50fabd790e
georgeerel <georgeerel1@gmail.com>
2025-07-03 18:20:36 +0300

    Changed to SparkMax

--- src/main/java/frc/robot/subsystems/FirstSubsystems.java
+++ src/main/java/frc/robot/subsystems/FirstSubsystems.java
@@ -1,17 +1,19 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class FirstSubsystems extends SubsystemBase{
-    private final TalonFX motor;
+    private final SparkMax motor;
 
     public FirstSubsystems(){
         super();
-        motor = new TalonFX(Constants.FirstSubsystems.MotorId,"one");
-        motor.setInverted(Constants.FirstSubsystems.MotorInverted);
+        motor = new SparkMax(Constants.FirstSubsystemsConstants.MotorId, MotorType.kBrushed)
+        motor.setInverted(Constants.FirstSubsystemsConstants.MotorInverted);
     }
     public void setPower(double power){
         motor.set(power);
