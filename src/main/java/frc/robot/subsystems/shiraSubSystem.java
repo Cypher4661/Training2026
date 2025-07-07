4bcba5c47974a5460e24e7301452bf64584b5c2c
shira-zada <zada.shira@gmail.com>
2025-07-07 18:08:35 +0300

    m

---
+++ src/main/java/frc/robot/subsystems/shiraSubSystem.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class shiraSubSystem extends SubsystemBase{
+    private SparkMax motor;
+
+    public shiraSubSystem(){
+        motor = new SparkMax(Constants.shiraConstants.motorID, MotorType.kBrushless);
+    }
+    public void setPower(double power){
+        motor.set(power);
+    }
+    public double getPosition(){
+        return motor.getEncoder().getPosition() * Constants.shiraConstants.ratio;
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("position", getPosition());
+    }
+}
