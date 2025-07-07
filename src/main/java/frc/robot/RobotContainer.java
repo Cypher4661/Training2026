8906376c7ac3591370e30c44420b5cb997e0b29e
shira-zada <zada.shira@gmail.com>
2025-07-07 18:52:57 +0300

    andthen

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import frc.robot.subsystems.shiraSubSystem;
 import org.ejml.data.CMatrix;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -23,8 +24,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private shiraSubSystem shira = new shiraSubSystem();
-  private Command ninety = new goToCommand(shira, 0.03, 90);
+  private Command ninety = new goToCommand(shira, 0.01, 90);
   private Command cmd = new shiraCommand(shira, 0.3, 4);
+  private Command fF = new goToCommand(shira, 0.01, 45);
+  private Command wate = new WaitCommand(5);
+  private Command goBack = new goToCommand(shira, -0.01, -135);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,6 +46,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return ninety;
+    return ninety.andThen(wate.andThen(fF.andThen(wate.andThen(goBack))));
   }
 }
