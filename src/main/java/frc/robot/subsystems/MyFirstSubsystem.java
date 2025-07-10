df3e44c906a6037ee57aa937354ee5822d7beea2
razco1212 <raztektok@gmail.com>
2025-07-10 19:11:21 +0300

    Increase power adjustments in GoToCommand and update position calculation in MyFirstSubsystem

--- src/main/java/frc/robot/subsystems/MyFirstSubsystem.java
+++ src/main/java/frc/robot/subsystems/MyFirstSubsystem.java
@@ -36,7 +36,7 @@ public class MyFirstSubsystem extends SubsystemBase {
   }
 
   public double getPosition() {
-    return motor.getEncoder().getPosition() * Constants.MyFirstSubsystem.MotorPositionRatio;
+    return motor.getEncoder().getPosition() * Constants.MyFirstSubsystem.MotorPositionRatio * 360;
   }
 
   @Override
