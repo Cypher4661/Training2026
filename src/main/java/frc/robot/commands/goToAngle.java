e3914e61766bad269390a3018d2f277cb3f9505a
georgeerel <georgeerel1@gmail.com>
2025-07-10 20:27:35 +0300

    fix goToAngle

--- src/main/java/frc/robot/commands/goToAngle.java
+++ src/main/java/frc/robot/commands/goToAngle.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.lang.ModuleLayer.Controller;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,28 +17,32 @@ public class goToAngle extends Command {
    private double Kd;
    private double sumErrors = 0.0;
    private double lastError = 0.0;
-   private PIDController controller = new PIDController(Kp, Ki, Kd);
+   private final PIDController controller;
 
    public goToAngle(FirstSubsystems subsystem){
       this.position = subsystem.getPosition();
       this.subsystem = subsystem;
+      controller = new PIDController(Kp, Ki, Kd);
       this.target = SmartDashboard.getNumber("Target", 0.0);
       Kp = SmartDashboard.getNumber("Kp", 0.0);
       Ki = SmartDashboard.getNumber("Ki", 0.0);
       Kd = SmartDashboard.getNumber("Kd", 0.0);
+      controller.setTolerance(2.0, 15.0);
+      controller.enableContinuousInput(-180, 180);
       addRequirements(subsystem);
    }
-    
+    public void initialize(){
+        controller.reset();
+        controller.setSetpoint(position);
+    }
 
     public void execute(){
-        controller.enableContinuousInput(-180, 180);
-        double Power = controller.calculate(position, target);
+        double Power = controller.calculate(position);
         subsystem.setPower(Power);
     }
     
     public boolean isFinished(){
-        double error = position-target;
-        return Math.abs(error) < 0;
+        return controller.atSetpoint();
     }
 
     public void end(boolean interrupted) {
