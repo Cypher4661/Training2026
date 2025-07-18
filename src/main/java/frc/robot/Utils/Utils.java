6df2456d45b0ceee9e18133abda2b25371de38f3
KislevUdi <udi.kislev@gmail.com>
2025-07-18 11:56:46 +0300

    Add Utils class for joystick value handling; update launch configuration

---
+++ src/main/java/frc/robot/Utils/Utils.java
@@ -0,0 +1,71 @@
+package frc.robot.Utils;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Utils {
+
+    public static double getJoystickX(XboxController controller, boolean isLeft, double maxValue, double minValue, double power) {
+        return getJoystickValue(isLeft ? controller.getLeftX() : controller.getRightX(), maxValue, minValue, power);
+    }
+
+    public static double getJoystickX(XboxController controller, boolean isLeft, double maxValue, double minValue) {
+        return getJoystickX(controller, isLeft, maxValue, minValue, 2);
+    }
+    public static double getJoystickX(XboxController controller, boolean isLeft, double maxValue) {
+        return getJoystickX(controller, isLeft, maxValue, 0.1, 2);
+    }
+    public static double getJoystickX(XboxController controller, boolean isLeft) {
+        return getJoystickX(controller, isLeft, 1, 0.1, 2);
+    }
+    public static double getJoystickLeftX(XboxController controller) {
+        return getJoystickX(controller, true, 1, 0.1, 2);
+    }
+    public static double getJoystickRightX(XboxController controller) {
+        return getJoystickX(controller, false, 1, 0.1, 2);
+    }
+    public static double getJoystickLeftX(XboxController controller, double maxValue) {
+        return getJoystickX(controller, true, maxValue, 0.1, 2);
+    }
+    public static double getJoystickRightX(XboxController controller, double maxValue) {
+        return getJoystickX(controller, false, maxValue, 0.1, 2);
+    }
+    
+    public static double getJoystickY(XboxController controller, boolean isLeft, double maxValue, double minValue, double power) {
+        return getJoystickValue(isLeft ? -controller.getLeftY() : -controller.getRightY(), maxValue, minValue, power);
+    }
+    public static double getJoystickY(XboxController controller, boolean isLeft, double maxValue, double minValue) {
+        return getJoystickY(controller, isLeft, maxValue, minValue, 2);
+    }
+    public static double getJoystickY(XboxController controller, boolean isLeft, double maxValue) {
+        return getJoystickY(controller, isLeft, maxValue, 0.1, 2);
+    }
+    public static double getJoystickY(XboxController controller, boolean isLeft) {
+        return getJoystickY(controller, isLeft, 1, 0.1, 2);
+    }
+    public static double getJoystickLeftY(XboxController controller) {
+        return getJoystickY(controller, true, 1, 0.1, 2);
+    }
+    public static double getJoystickRightY(XboxController controller) {
+        return getJoystickY(controller, false, 1, 0.1, 2);
+    }
+    public static double getJoystickLeftY(XboxController controller, double maxValue) {
+        return getJoystickY(controller, true, maxValue, 0.1, 2);
+    }
+    public static double getJoystickRightY(XboxController controller, double maxValue) {
+        return getJoystickY(controller, false, maxValue, 0.1, 2);
+    }
+
+    public static double getJoystickValue(double value, double maxValue, double minValue, double power) {
+        if(Math.abs(value) < minValue) {
+            return 0;
+        }
+        value = Math.pow(Math.abs(value), power) * Math.signum(value);
+        return value * maxValue;
+    }
+
+    public static void main(String[] args) {
+        for(double d = -1; d <= 1; d+=0.1)
+            System.out.printf("%4.2f -> %4.2f\n", d, getJoystickValue(d,0.7, 0.1, 1.5));
+    }
+
+}
--- src/main/java/frc/robot/subsystems/FirstSubsystem.java
+++ src/main/java/frc/robot/subsystems/FirstSubsystem.java
@@ -15,7 +15,9 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Utils.Utils;
 import frc.robot.commands.GoToAnglePID;
 
 import static edu.wpi.first.units.Units.Degrees;
@@ -34,6 +36,8 @@ public class FirstSubsystem extends SubsystemBase {
         configureTalonMotor();
         SmartDashboard.putData("FirstSubsystem", this);
         SmartDashboard.putData("follow", new GoToAnglePID(this));
+        SmartDashboard.putNumber("TargetPower", 0);
+        SmartDashboard.putData("Manual Power", new RunCommand(()->setPower(SmartDashboard.getNumber("TargetPower", 0)), this));
     }
 
     private void configureMotor() {
