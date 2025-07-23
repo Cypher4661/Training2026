4f5929209fc78d6820262d44ee7a76bcf66a5fa9
original2011 <oriyuda810@gmail.com>
2025-07-23 21:21:28 +0300

    add pid and feedforward

--- src/main/java/frc/robot/subsystems/ModoleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ModoleSubsystem.java
@@ -9,9 +9,10 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -19,83 +20,92 @@ public class ModoleSubsystem extends SubsystemBase {
     private final SparkMax steerMotor;
     private final SparkMax driveMotor;
     private final CANcoder canCoder;
-  
-  public ModoleSubsystem() {
-    super();
-    steerMotor = new SparkMax(Constants.steerMotorConstants.steerMotorID, MotorType.kBrushless);
-    driveMotor = new SparkMax(Constants.driveMotorConstants.driveMotorID, MotorType.kBrushless);
-    canCoder = new CANcoder(Constants.MyFirstSubsystemConstants.CANcoderId);
-    SmartDashboard.putData("Modole", this);
-    var cfg = new SparkMaxConfig();
-    cfg.inverted(Constants.steerMotorConstants.steerMotorInverted);
-    steerMotor.configure(cfg, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
-    var cfg2 = new SparkMaxConfig(); 
-    cfg2.inverted(Constants.driveMotorConstants.driveMotorInverted);
-    driveMotor.configure(cfg2, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
-    SmartDashboard.putData("set steer 0.3", new StartEndCommand(()->setSteerMotorPower(0.3), ()->setSteerMotorPower(0), this));
-    SmartDashboard.putData("set steer 0.4", new StartEndCommand(()->setSteerMotorPower(0.4), ()->setSteerMotorPower(0), this));
-    SmartDashboard.putData("set steer 0.3", new StartEndCommand(()->setDriveMotorPower(0.3), ()->setDriveMotorPower(0), this));
-    SmartDashboard.putData("set steer 0.4", new StartEndCommand(()->setDriveMotorPower(0.4), ()->setDriveMotorPower(0), this));
-    // Initialization code can be added here if needed.
-    setEncoder(getAbseloteAngele() - Constants.MyFirstSubsystemConstants.CANofset);
-    }
-    public void setSteerMotorPower(double power) {
-        steerMotor.set(power);
-    }
-    public void setDriveMotorPower(double power) {
-        driveMotor.set(power);
-    }
-    public double getSteerPower() {
-        return steerMotor.getAppliedOutput();
-      }
-    public double getdriverPower() {
-        return driveMotor.getAppliedOutput();
-    }
-    public double getSteerMotorPosition() {
-        double Angle = steerMotor.getEncoder().getPosition() / Constants.steerMotorConstants.steerMotorGearRatio * 360;
-        return MathUtil.inputModulus(Angle, -180, 180);
-    }
-    public double getDriveMotorPosition() {
-        return driveMotor.getEncoder().getPosition() / Constants.driveMotorConstants.driveMotorGearRatio * 360;
-    }
-    public void stopSteerMotor() {
-        setSteerMotorPower(0);
-    }
-    public void stopDriveMotor() {
-        setDriveMotorPower(0);
-    }
-    public double getSteerMotorVelocity() {
-        return steerMotor.getEncoder().getVelocity() * Constants.steerMotorConstants.steerMotorGearRatio * 360 / 60;
-    }
-    public double getdriveMotorVelocity() {
-        return driveMotor.getEncoder().getVelocity() * Constants.driveMotorConstants.driveMotorGearRatio / 60 * Math.PI*Constants.MyFirstSubsystemConstants.diameter;
-    }
-    public void setSteerMotorVelocity(double velocity) {
-    }
-    public void setDriveMotorVelocity(double velocity) {
-    }
-    public double getAbseloteAngele() {
-        return canCoder.getAbsolutePosition().getValueAsDouble() * 360;
-    }
-    public void setEncoder(double Angle) {
-        steerMotor.getEncoder().setPosition(Angle);
-    }
-    @Override
-    public void periodic() {
-    // This method can be overridden to add periodic tasks for this subsystem.
 
-    }
-    @Override
-    public void initSendable(SendableBuilder builder) {
+    private final SimpleMotorFeedforward sFeedforward = new SimpleMotorFeedforward(0.0075,0.000625);
+    private final PIDController sPIDController = new PIDController(0.1, 0, 0.01);
+    private final SimpleMotorFeedforward dFeedforward = new SimpleMotorFeedforward(1.0/150.0,2.0/9.0);
+    private final PIDController dPIDController = new PIDController(0.1, 0, 0.01);
+
+    public ModoleSubsystem() {
+        super();
+        steerMotor = new SparkMax(Constants.steerMotorConstants.steerMotorID, MotorType.kBrushless);
+        driveMotor = new SparkMax(Constants.driveMotorConstants.driveMotorID, MotorType.kBrushless);
+        canCoder = new CANcoder(Constants.MyFirstSubsystemConstants.CANcoderId);
+        SmartDashboard.putData("Modole", this);
+        var cfg = new SparkMaxConfig();
+        cfg.inverted(Constants.steerMotorConstants.steerMotorInverted);
+        steerMotor.configure(cfg, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        var cfg2 = new SparkMaxConfig(); 
+        cfg2.inverted(Constants.driveMotorConstants.driveMotorInverted);
+        driveMotor.configure(cfg2, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        // Initialization code can be added here if needed.
+
+        }
+        public void setSteerMotorPower(double power) {
+            steerMotor.set(power);
+        }
+        public void setDriveMotorPower(double power) {
+            driveMotor.set(power);
+        }
+        public double getSteerPower() {
+            return steerMotor.getAppliedOutput();
+        }
+        public double getdriverPower() {
+            return driveMotor.getAppliedOutput();
+        }
+        public double getSteerMotorPosition() {
+            double Angle = steerMotor.getEncoder().getPosition() / Constants.steerMotorConstants.steerMotorGearRatio * 360;
+            return MathUtil.inputModulus(Angle, -180, 180);
+        }
+        public double getDriveMotorPosition() {
+            return driveMotor.getEncoder().getPosition() / Constants.driveMotorConstants.driveMotorGearRatio * 360;
+        }
+        public void stopSteerMotor() {
+            setSteerMotorPower(0);
+        }
+        public void stopDriveMotor() {
+            setDriveMotorPower(0);
+        }
+        public double getSteerMotorVelocity() {
+            return steerMotor.getEncoder().getVelocity() * Constants.steerMotorConstants.steerMotorGearRatio * 360 / 60;
+        }
+        public double getdriveMotorVelocity() {
+            return driveMotor.getEncoder().getVelocity() * Constants.driveMotorConstants.driveMotorGearRatio / 60 * Math.PI*Constants.MyFirstSubsystemConstants.diameter;
+        }
+        public void setSteerMotorVelocity(double velocity) {
+            double ff = sFeedforward.calculate(velocity);
+            double pid = sPIDController.calculate(getSteerMotorPosition(), velocity);
+            setSteerMotorPower(ff + pid);
+        }
+        public void setDriveMotorVelocity(double velocity) {
+            double ff = dFeedforward.calculate(velocity);
+            double pid = dPIDController.calculate(getDriveMotorPosition(), velocity);
+            setDriveMotorPower(ff + pid);
+        }
+        public double getAbseloteAngele() {
+            return canCoder.getAbsolutePosition().getValueAsDouble() * 360;
+        }
+        public void setEncoder(double Angle) {
+            steerMotor.getEncoder().setPosition(Angle);
+        }
+        @Override
+        public void periodic() {
+        // This method can be overridden to add periodic tasks for this subsystem.
+
+        }
+        @Override
+        public void initSendable(SendableBuilder builder) {
         // TODO Auto-generated method stub
-        super.initSendable(builder);
-        builder.addDoubleProperty("Steer Motor Position", this::getSteerMotorPosition, null);
-        builder.addDoubleProperty("Drive Motor Position", this::getDriveMotorPosition, null);
-        builder.addDoubleProperty("Steer Motor Velocity", this::getSteerMotorVelocity, null);
-        builder.addDoubleProperty("Drive Motor Velocity", this::getdriveMotorVelocity, null);
-        builder.addDoubleProperty("Steer Motor Power", this::getSteerPower, null);
-        builder.addDoubleProperty("Drive Motor Power", this::getdriverPower, null);
-        builder.addDoubleProperty("Abselote Angle", this::getAbseloteAngele, null);
+            super.initSendable(builder);
+            builder.addDoubleProperty("Steer Motor Position", this::getSteerMotorPosition, null);
+            builder.addDoubleProperty("Drive Motor Position", this::getDriveMotorPosition, null);
+            builder.addDoubleProperty("Steer Motor Velocity", this::getSteerMotorVelocity, null);
+            builder.addDoubleProperty("Drive Motor Velocity", this::getdriveMotorVelocity, null);
+            builder.addDoubleProperty("Steer Motor Power", this::getSteerPower, null);
+            builder.addDoubleProperty("Drive Motor Power", this::getdriverPower, null);
+            builder.addDoubleProperty("Abselote Angle", this::getAbseloteAngele, null);
+            
     }
 }
 
