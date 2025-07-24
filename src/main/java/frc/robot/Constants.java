a29faf72c1894539c186abd0a41ebdee7a42a564
YonatanCohen4661 <yonatan9729@gmail.com>
2025-07-24 18:59:45 +0300

    gg ewel plaid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,10 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.CANBus;
+
+import frc.robot.utils.SparkConfig;
+import frc.robot.utils.TalonConfig;
 
 /**
 * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -29,6 +33,27 @@ public final class Constants {
 
 
    }
+
+   public static class Example {
+    public static final TalonConfig TALON_CONFIG = new TalonConfig(7,new CANBus("rio"), "talon example motor")
+          .withBrake(true)
+          .withCurrent(20)
+          .withInvert(true)
+          .withMeterMotor(12.7, 4*0.0254)
+          .withVelocities(3, 6, 10)
+          .withPID(1, 0, 0, 0.12, 3.7, 1.2, 0)
+          .withRampTime(0.3)
+          .withVolts(6);
+    public static final SparkConfig SPARKMOTOR_CONFIG = new SparkConfig(8, "spark example motor")
+        .withBrake(true)
+        .withCurrent(20)
+        .withInvert(false)
+        .withPID(2,   0.2,   0, 0.1)
+        .withDegreesMotor(8.4)
+        .withVelocities(720, 1000, 3000)
+        .withRampTime(0.2)
+        .withVolts(8);
+  }
 }
 
 
