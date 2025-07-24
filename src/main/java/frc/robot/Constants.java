43602e7c0b3e076272d06978f5f762c35aa75b17
razco1212 <raztektok@gmail.com>
2025-07-24 19:12:57 +0300

    const

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,27 +9,7 @@ import frc.robot.utils.SparkConfig;
 import frc.robot.utils.TalonConfig;
 
 public final class Constants {
-  public static class Example {
-    public static final TalonConfig TALON_CONFIG = new TalonConfig(7,new CANBus("rio"), "talon example motor")
-          .withBrake(true)
-          .withCurrent(20)
-          .withInvert(true)
-          .withMeterMotor(12.7, 4*0.0254)
-          .withVelocities(3, 6, 10)
-          .withPID(1, 0, 0, 0.12, 3.7, 1.2, 0)
-          .withRampTime(0.3)
-          .withVolts(6);
-    public static final SparkConfig SPARKMOTOR_CONFIG = new SparkConfig(8, "spark example motor")
-        .withBrake(true)
-        .withCurrent(20)
-        .withInvert(false)
-        .withPID(2,   0.2,   0, 0.1)
-        .withDegreesMotor(8.4)
-        .withVelocities(720, 1000, 3000)
-        .withRampTime(0.2)
-        .withVolts(8);
-  }
-
+  
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
@@ -57,6 +37,27 @@ public final class Constants {
 
     public static final double DriverDeadband = 0.1;
   }
+  public static class Example {
+    public static final TalonConfig TALON_CONFIG = new TalonConfig(7,new CANBus("rio"), "talon example motor")
+          .withBrake(true)
+          .withCurrent(20)
+          .withInvert(true)
+          .withMeterMotor(12.7, 4*0.0254)
+          .withVelocities(3, 6, 10)
+          .withPID(1, 0, 0, 0.12, 3.7, 1.2, 0)
+          .withRampTime(0.3)
+          .withVolts(6);
+    public static final SparkConfig SPARKMOTOR_CONFIG = new SparkConfig(8, "spark example motor")
+        .withBrake(true)
+        .withCurrent(20)
+        .withInvert(false)
+        .withPID(2,   0.2,   0, 0.1)
+        .withDegreesMotor(8.4)
+        .withVelocities(720, 1000, 3000)
+        .withRampTime(0.2)
+        .withVolts(8);
+  }
+
 
 
 }
