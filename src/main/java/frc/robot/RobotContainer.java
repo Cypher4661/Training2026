4e1ba7184b62523e3fcb47aa068b053493b82bfb
razco1212 <raztektok@gmail.com>
2025-08-11 15:42:06 +0300

    Add SwerveSubsystem initialization in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-
+import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -17,7 +17,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  // The robot's subsystems and commands are defined here... 
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
   
   private CommandXboxController controller = new CommandXboxController(Constants.OperatorConstants.kDriverControllerPort);
   
